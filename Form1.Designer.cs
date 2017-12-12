c7024c42dc9e4d309f959839df1bee6fabaf49fb
Rafael Baptista <rafabaptista@users.noreply.github.com>
2017-12-11 22:14:41 -0200

    Sorteio
    Sorteio C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ULA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblAddLista1 = new System.Windows.Forms.Label();
+            this.lblAddLista2 = new System.Windows.Forms.Label();
+            this.txtAddList1 = new System.Windows.Forms.TextBox();
+            this.txtAddList2 = new System.Windows.Forms.TextBox();
+            this.lstLista1 = new System.Windows.Forms.ListBox();
+            this.lstLista2 = new System.Windows.Forms.ListBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.btnClearLista2 = new System.Windows.Forms.Button();
+            this.btnClearLista1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAddLista1
+            // 
+            this.lblAddLista1.AutoSize = true;
+            this.lblAddLista1.Location = new System.Drawing.Point(37, 29);
+            this.lblAddLista1.Name = "lblAddLista1";
+            this.lblAddLista1.Size = new System.Drawing.Size(108, 13);
+            this.lblAddLista1.TabIndex = 0;
+            this.lblAddLista1.Text = "ADICIONAR LISTA 1";
+            // 
+            // lblAddLista2
+            // 
+            this.lblAddLista2.AutoSize = true;
+            this.lblAddLista2.Location = new System.Drawing.Point(460, 29);
+            this.lblAddLista2.Name = "lblAddLista2";
+            this.lblAddLista2.Size = new System.Drawing.Size(108, 13);
+            this.lblAddLista2.TabIndex = 1;
+            this.lblAddLista2.Text = "ADICIONAR LISTA 2";
+            // 
+            // txtAddList1
+            // 
+            this.txtAddList1.Location = new System.Drawing.Point(40, 56);
+            this.txtAddList1.Name = "txtAddList1";
+            this.txtAddList1.Size = new System.Drawing.Size(246, 20);
+            this.txtAddList1.TabIndex = 2;
+            this.txtAddList1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAddList1_KeyDown);
+            // 
+            // txtAddList2
+            // 
+            this.txtAddList2.Location = new System.Drawing.Point(463, 56);
+            this.txtAddList2.Name = "txtAddList2";
+            this.txtAddList2.Size = new System.Drawing.Size(246, 20);
+            this.txtAddList2.TabIndex = 3;
+            this.txtAddList2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAddList2_KeyDown);
+            // 
+            // lstLista1
+            // 
+            this.lstLista1.FormattingEnabled = true;
+            this.lstLista1.Location = new System.Drawing.Point(40, 100);
+            this.lstLista1.Name = "lstLista1";
+            this.lstLista1.Size = new System.Drawing.Size(246, 641);
+            this.lstLista1.TabIndex = 4;
+            this.lstLista1.SelectedIndexChanged += new System.EventHandler(this.lstLista1_SelectedIndexChanged);
+            // 
+            // lstLista2
+            // 
+            this.lstLista2.FormattingEnabled = true;
+            this.lstLista2.Location = new System.Drawing.Point(463, 100);
+            this.lstLista2.Name = "lstLista2";
+            this.lstLista2.Size = new System.Drawing.Size(246, 641);
+            this.lstLista2.TabIndex = 5;
+            this.lstLista2.SelectedIndexChanged += new System.EventHandler(this.lstLista2_SelectedIndexChanged);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(292, 100);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(165, 142);
+            this.btnExecute.TabIndex = 6;
+            this.btnExecute.Text = "SORTEAR";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // btnClearLista2
+            // 
+            this.btnClearLista2.Location = new System.Drawing.Point(463, 777);
+            this.btnClearLista2.Name = "btnClearLista2";
+            this.btnClearLista2.Size = new System.Drawing.Size(246, 29);
+            this.btnClearLista2.TabIndex = 7;
+            this.btnClearLista2.Text = "LIMPAR LISTA 2";
+            this.btnClearLista2.UseVisualStyleBackColor = true;
+            this.btnClearLista2.Click += new System.EventHandler(this.btnClearLista2_Click);
+            // 
+            // btnClearLista1
+            // 
+            this.btnClearLista1.Location = new System.Drawing.Point(40, 777);
+            this.btnClearLista1.Name = "btnClearLista1";
+            this.btnClearLista1.Size = new System.Drawing.Size(246, 29);
+            this.btnClearLista1.TabIndex = 8;
+            this.btnClearLista1.Text = "LIMPAR LISTA 1";
+            this.btnClearLista1.UseVisualStyleBackColor = true;
+            this.btnClearLista1.Click += new System.EventHandler(this.btnClearLista1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 836);
+            this.Controls.Add(this.btnClearLista1);
+            this.Controls.Add(this.btnClearLista2);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.lstLista2);
+            this.Controls.Add(this.lstLista1);
+            this.Controls.Add(this.txtAddList2);
+            this.Controls.Add(this.txtAddList1);
+            this.Controls.Add(this.lblAddLista2);
+            this.Controls.Add(this.lblAddLista1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sorteio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAddLista1;
+        private System.Windows.Forms.Label lblAddLista2;
+        private System.Windows.Forms.TextBox txtAddList1;
+        private System.Windows.Forms.TextBox txtAddList2;
+        private System.Windows.Forms.ListBox lstLista1;
+        private System.Windows.Forms.ListBox lstLista2;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Button btnClearLista2;
+        private System.Windows.Forms.Button btnClearLista1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+namespace ULA
+{
+    public partial class Form1 : Form
+    {
+        #region .: Constructor :.
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region .: Events :.
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckButtonVisibility();
+        }
+
+        private void txtAddList1_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyValue.Equals(13))
+                    AddInList1();
+
+                CheckButtonVisibility();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txtAddList2_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyValue.Equals(13))
+                    AddInList2();
+
+                CheckButtonVisibility();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DoSorteio();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
+        private void btnClearLista1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstLista1.SelectedItem != null)
+                {
+                    lstLista1.Items.Remove(lstLista1.SelectedItem.ToString());
+                    txtAddList1.Focus();
+                }
+                else
+                {
+                    lstLista1.Items.Clear();
+                    txtAddList1.Focus();
+                }
+
+                CheckButtonVisibility();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClearLista2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstLista2.SelectedItem != null)
+                {
+                    lstLista2.Items.Remove(lstLista2.SelectedItem.ToString());
+                    txtAddList2.Focus();
+                }
+                else
+                {
+                    lstLista2.Items.Clear();
+                    txtAddList2.Focus();
+                }
+
+                CheckButtonVisibility();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region .: Methods :.
+
+        private void AddInList1()
+        {
+            string valueInput = txtAddList1.Text.Trim().ToUpper();
+
+            if (!lstLista1.Items.Contains(valueInput))
+            {
+                lstLista1.Items.Add(valueInput);
+                txtAddList1.Text = "";
+                txtAddList1.Focus();
+            }
+            else
+            {
+                MessageBox.Show("A Lista 1 Ja contem esse item.");
+                txtAddList1.Text = "";
+                txtAddList1.Focus();
+            }
+        }
+
+        private void AddInList2()
+        {
+            string valueInput = txtAddList2.Text.Trim().ToUpper();
+
+            if (!lstLista2.Items.Contains(valueInput))
+            {
+                lstLista2.Items.Add(valueInput);
+                txtAddList2.Text = "";
+                txtAddList2.Focus();
+            }
+            else
+            {
+                MessageBox.Show("A Lista 2 Ja contem esse item.");
+                txtAddList2.Text = "";
+                txtAddList2.Focus();
+            }
+        }
+
+        private void DoSorteio()
+        {
+            List<string> allLista1 = new List<string>();
+            List<string> allLista2 = new List<string>();
+            List<int> ordemLista1 = new List<int>();
+            List<int> ordemLista2 = new List<int>();
+            int totalLista1 = lstLista1.Items.Count;
+            int totalLista2 = lstLista2.Items.Count;
+            int increment = 0;
+            string result = "";
+            Random rdnGetNumeros = new Random();
+            int numeroRamdom;
+            int checkListaMenor = ((totalLista1 > totalLista2) ? totalLista2 : totalLista1);
+            
+            foreach (string item in lstLista1.Items)
+            {
+                allLista1.Add(item);
+            }
+
+            foreach (string item in lstLista2.Items)
+            {
+                allLista2.Add(item);
+            }
+
+            if(allLista1.Count > 0 && allLista2.Count > 0)
+            {
+                while (increment < checkListaMenor)
+                {
+                    numeroRamdom = rdnGetNumeros.Next(totalLista1);
+
+                    if (!ordemLista1.Contains(numeroRamdom))
+                    {
+                        ordemLista1.Add(numeroRamdom);
+                        increment++;
+                    }
+                }
+
+                increment = 0;
+
+                while (increment < checkListaMenor)
+                {
+                    numeroRamdom = rdnGetNumeros.Next(totalLista2);
+
+                    if (!ordemLista2.Contains(numeroRamdom))
+                    {
+                        ordemLista2.Add(numeroRamdom);
+                        increment++;
+                    }
+                }
+
+                result = "\nResultado do Sorteio:\n\n";
+
+                for (int i = 0; i < checkListaMenor; i++)
+                {
+                    result += lstLista1.Items[ordemLista1[i]].ToString() + " - " + lstLista2.Items[ordemLista2[i]].ToString() + " \n";
+                }
+            }
+            else
+            {
+                numeroRamdom = rdnGetNumeros.Next((totalLista1 > 0 && totalLista2 == 0) ? totalLista1 : totalLista2);
+
+                result = "\nResultado do Sorteio:\n\n";
+                result += (totalLista1 > 0 && totalLista2 == 0) ? lstLista1.Items[numeroRamdom].ToString() : lstLista2.Items[numeroRamdom].ToString();
+
+                result += " !! ";
+            }
+            
+            MessageBox.Show(result, "SORTEIO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void lstLista2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstLista2.SelectedItem != null)
+                {
+                    btnClearLista2.Text = "Remover item selecionado Lista 2";
+                }
+                else
+                {
+                    btnClearLista2.Text = "Limpar Lista 2";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void lstLista1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lstLista1.SelectedItem != null)
+                {
+                    btnClearLista1.Text = "Remover item selecionado Lista 1";
+                }
+                else
+                {
+                    btnClearLista1.Text = "Limpar Lista 1";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        #endregion
+
+        private void CheckButtonVisibility()
+        {
+            btnClearLista1.Visible = (lstLista1.Items.Count > 0) ? true : false;
+            btnClearLista2.Visible = (lstLista2.Items.Count > 0) ? true : false;
+        }
+    }
+}
